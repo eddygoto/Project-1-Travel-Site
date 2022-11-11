7e4de7c86e4acdf59bbaa5dd10aca0837b125518
Ryan Fernandez <ryanfernandez11@gmail.com>
2022-11-10 19:33:32 -0800

    added multiple markers and called all data points from api on marker click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
-var grandOle, grandOleName, grandOleId, grandOleLat, grandOleLng;
-
 var hiddenGems = [
-    "Grand Ole",
+    "Grand Ole BBQ",
     "La Fachada",
     "Trining's Bakery",
-    "Liberty Public Market",
-    "Morley Field Disc Golf Course",
-    "OB Noodle House Bar 1502",
+    "Swami's Beach"
+    // "Liberty Public Market"
+    // ------ Coming up with OVER_QUERY_LIMIT when running getDetails for more than four places
+    // "Morley Field Disc Golf Course",
+    // "OB Noodle House Bar 1502",
     // "Campfire"
 ];
 
 
 var reviews;
-var placeReviewsEl;
-window.onload = function () {
-    placeReviewsEl = document.getElementById("place-reviews")
-    displayReviews();
-}
+var placeReviewsEl = document.getElementById("place-reviews");
+var placeDetailsEl = document.querySelector("#place-details");
+var placeNameEl = document.querySelector("#place-name");
+
 function initMap() {
     const map = new google.maps.Map(
         document.getElementById("map"),
@@ -26,73 +25,92 @@ function initMap() {
         }
     );
 
-    /*************** Request information for findPlaceFromQuery ******************/
-    var fPFQRequest = {
-        query: hiddenGems[0], // search term query
-        fields: ['name', 'geometry', 'place_id'],   // fields that we want API request to return
-    }
-
     const infowindow = new google.maps.InfoWindow();
     const service = new google.maps.places.PlacesService(map);
 
-    service.findPlaceFromQuery(fPFQRequest, function (results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < hiddenGems.length; i++) {
 
-            /*************** Request information for call to getDetails() ******************/
-            getDetailsRequest = {
-                placeId: results[0].place_id,   // getDetails searches by place_id
-                fields: ["name", "formatted_address", "place_id", "geometry", "reviews"],   // fields to return
-            };
+        /*************** Request information for findPlaceFromQuery ******************/
+        var fPFQRequest = {
+            query: hiddenGems[i], // search term query
+            fields: ['name', 'geometry', 'place_id'],   // fields that we want API request to return
+        }
 
-            service.getDetails(getDetailsRequest, (place, status) => {
-                if (
+       
+
+        service.findPlaceFromQuery(fPFQRequest, function(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+
+                console.log(results[0]); 
+
+                /*************** Request information for call to getDetails() ******************/
+                getDetailsRequest = {
+                    placeId: results[0].place_id,   // getDetails searches by place_id
+                    fields: ["name", "formatted_address", "place_id", "geometry", "reviews"],   // fields to return
+                };
+
+                service.getDetails(getDetailsRequest, (place, status) => {
+                    if (
                     status === google.maps.places.PlacesServiceStatus.OK &&
                     place &&
                     place.geometry &&
                     place.geometry.location
-                ) {
-                    /*************** Placing a Marker ******************/
-                    const marker = new google.maps.Marker({
-                        map,
-                        position: place.geometry.location,
-                    });
+                    ) {
+                        /*************** Placing a Marker ******************/
+                        const marker = new google.maps.Marker( {
+                            map,
+                            position: place.geometry.location,
+                        });
 
-                    /*************** Event Listener Code ******************/
-                    google.maps.event.addListener(marker, "click", () => {
-                        const content = document.createElement("div");
+                        /*************** Event Listener Code ******************/
+                        google.maps.event.addListener(marker, "click", () => {
+                            const content = document.createElement("div");
 
-                        const nameElement = document.createElement("h2");
-                        nameElement.textContent = place.name;
-                        content.appendChild(nameElement);
+                            const nameElement = document.createElement("h2");
+                            nameElement.textContent = place.name;
+                            content.appendChild(nameElement);
 
-                        const placeAddressElement = document.createElement("p");
-                        placeAddressElement.textContent = place.formatted_address;
-                        content.appendChild(placeAddressElement);
+                            const placeAddressElement = document.createElement("p");
+                            placeAddressElement.textContent = place.formatted_address;
+                            content.appendChild(placeAddressElement);
 
-                        infowindow.setContent(content);
-                        infowindow.open(map, marker);
-                        
-                    });
+                            infowindow.setContent(content);
+                            infowindow.open(map, marker);
 
-                    reviews = place.reviews; // store reviews outside of initMap() if needed
-                    console.log(reviews)
-                }
-            });
-        }
-    });
+                            cleanElement(placeReviewsEl);
+                            cleanElement(placeDetailsEl);
+                            displayDetails(place.name, place.formatted_address);
+                            displayReviews(place.reviews);
+                        });
+
+                        // reviews = place.reviews;    // store reviews outside of initMap() if needed
+                    }
+                    else {
+                        console.log("Error in getDetails:", status);
+                    }
+                });
+            }
+        });
+    }
+    
+}
+
+function cleanElement(element) {
+    while(element.children.length) {
+        element.removeChild(element.children[0]);
+    }
 }
 
 /**************Display Details Code***************** */
-function displayDetails() {
-    const placeDetailsEl = document.querySelector("#place-details")
-    const placeNameEl = document.querySelector("#place-name")
-    placeNameEl.textContent = place.name;
-    placeDetailsEl.textContent = place.formatted_address;
+function displayDetails(name, address) {
+    
+    placeNameEl.textContent = name;
+    placeDetailsEl.textContent = address;
 }
 
 
 /**************Display Reviews Code***************** */
-function displayReviews() {
+function displayReviews(reviews) {
     for (var i = 0; i < 3; i++) {
         const reviewContainer = document.createElement("div");
         placeReviewsEl.appendChild(reviewContainer);
